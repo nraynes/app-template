8c269068f7685ecf3f55190ce53c6f75899e4b09
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2022-07-09 17:19:15 -0600

    added some forgot password e2e tests

--- frontend/cypress/e2e/forgotPassword.cy.js
+++ frontend/cypress/e2e/forgotPassword.cy.js
@@ -1,2 +1,24 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
+
+describe('Forgot Password feature tests', () => {
+
+  it('Should display the forgot password form when navigated to.', () => {
+    cy.exec('cd ../backend && npm run test:teardown')
+    cy.exec('cd ../backend && npm run test:setup')
+    cy.visit('http://localhost:3000/')
+
+    cy.get('[data-testid="login-button"]')
+      .click()
+
+    cy.get('[data-testid="login-form"]')
+      .should('exist')
+    
+    cy.get('[data-testid="login-form-forgot-password-button"]')
+      .click()
+
+    cy.get('[data-testid="forgot-password"]')
+      .should('exist')
+  })
+
+})
