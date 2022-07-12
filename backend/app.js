a448dd4e20936b16524f3338ee38e35aad37b7b0
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2022-07-12 15:37:31 -0600

    changed csp to allow for data loading image

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(
         // eslint-disable-next-line quotes
         "script-src": ["'self'", "https://www.google.com", "https://www.gstatic.com"],
         "frame-src": ["'self'", "https://www.google.com", "https://www.gstatic.com"],
-        "img-src": ["'self'", "https://www.google.com", "https://www.gstatic.com"],
+        "img-src": ["'self'", 'data:', "https://www.google.com", "https://www.gstatic.com"],
       },
     },
   }),
