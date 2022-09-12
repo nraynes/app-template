0713f5d0352cb823c7a30b9ae7890c280c3d100d
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2022-09-12 08:31:19 -0600

    Made it so app can run using docker compose

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+############### ▼ Build Stage ▼ ###############
+
+FROM node:lts AS builder
+
+ENV APP_HOME=/usr/src/app
+WORKDIR $APP_HOME
+
+COPY package.json package-lock.json ./
+COPY backend/package.json backend/package-lock.json backend/src/db/prisma ./backend/
+COPY frontend/package.json frontend/package-lock.json ./frontend/
+RUN npm run install:all
+
+COPY --chown=node:node . .
+RUN npm run build:all
+
+############### ▼ Run Stage ▼ ###############
+
+FROM node:lts
+
+ENV APP_HOME=/usr/src/app
+WORKDIR $APP_HOME
+
+USER node
+
+COPY --from=builder /etc/localtime /etc/localtime
+COPY --from=builder /etc/timezone /etc/timezone
+COPY --from=builder $APP_HOME $APP_HOME
--- frontend/src/providers/GlobalProvider.jsx
+++ frontend/src/providers/GlobalProvider.jsx
@@ -18,15 +18,15 @@ function GlobalProvider({ children }) {
   const auth = useAuth();
 
   // Reroute to https if not already on it.
-  const location = window.location.href;
-  if (
-    location.substring(0,5) !== 'https'
-    && process.env.NODE_ENV !== 'development'
-  ) {
-    const url = location.substring(5,location.length);
-    const newLocation = `https:${url}`;
-    window.location.href = newLocation;
-  }
+  // const location = window.location.href;
+  // if (
+  //   location.substring(0,5) !== 'https'
+  //   && process.env.NODE_ENV !== 'development'
+  // ) {
+  //   const url = location.substring(5,location.length);
+  //   const newLocation = `https:${url}`;
+  //   window.location.href = newLocation;
+  // }
 
   const pubRoutes = publicRoutes.map((item) => item.path.substring(0, item.path.length-2));
 
